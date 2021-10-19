bb7ced337dde9917d3a952c388f69b88bc56b438
OlegLR <oleglr@mail.ru>
2021-10-19 17:24:24 +0300

    30202

--- src/config.js
+++ src/config.js
@@ -26,7 +26,7 @@ const binary_desktop_app_id = 14473;
 
 const getAppId = () => {
     let app_id = null;
-    const user_app_id   = '29974'; // you can insert Application ID of your registered application here
+    const user_app_id   = '30202'; // you can insert Application ID of your registered application here
     const config_app_id = window.localStorage.getItem('config.app_id');
     if (config_app_id) {
         app_id = config_app_id;
